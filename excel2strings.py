0f08ffe4ae1c4b2bb3c9c1d4c100703585ffe104
Leo <iPhilly@users.noreply.github.com>
2020-04-20 19:39:19 +0800

    replace special characters

--- excel2strings.py
+++ excel2strings.py
@@ -5,7 +5,7 @@ import xlrd
 excel_file_name = "nls.xlsx"
 # 指定sheets名字s
 sheet_names = ["Reset secondary password", "Reset password", "Error handling", "Device optional page"]
-sheet_key_prefixs = ["reset_password_with_", "reset_secondary_password_with_", "reset_password_error_handing_with_", "device_optional_with_"]
+sheet_key_prefixs = ["", "", "", ""]
 
 #### 指定关键行号
 # 标题行的行号(没用使用)
@@ -40,38 +40,37 @@ for sheet_index in range(len(sheet_names)):
     values_tc = sheet.col_values(columnIndexForValues_tc, rowIndexForStartKeyValue)
 
     # 写入文件 EN
-    filename_en = 'output_' + sheet_name + '_en.strings'
+    filename_en = sheet_name + '_en.strings'
     with open(filename_en, 'w') as file_object:
-        file_object.write("/* %s */\n" % (key_prefix))
+        file_object.write("/* %s */\n" % (sheet_name))
         for index in range(len(keys)):
             key_text = keys[index]
-            value_text = values_en[index]
+            value_text = values_en[index].replace("\"", "\\\"").replace("\n", "\\n").replace("\\n\"", "\"")
             # 如果KEY内容为空(""),将被跳过
             if key_text != "":
                 text_line = "\"%s%s\" = \"%s\"" % (key_prefix,key_text, value_text)
                 file_object.write(text_line + ";\n")
                 
     # 写入文件 SC
-    filename_sc = 'output_' + sheet_name + '_sc.strings'
+    filename_sc = sheet_name + '_sc.strings'
     with open(filename_sc, 'w') as file_object:
-        file_object.write("/* %s */\n" % (key_prefix))
+        file_object.write("/* %s */\n" % (sheet_name))
         for index in range(len(keys)):
             key_text = keys[index]
-            value_text = values_sc[index]
+            value_text = values_sc[index].replace("\"", "\\\"").replace("\n", "\\n").replace("\\n\"", "\"")
             # 如果KEY内容为空(""),将被跳过
             if key_text != "":
                 text_line = "\"%s%s\" = \"%s\"" % (key_prefix,key_text, value_text)
                 file_object.write(text_line + ";\n")
 
     # 写入文件 TC
-    filename_tc = 'output_' + sheet_name + '_tc.strings'
+    filename_tc = sheet_name + '_tc.strings'
     with open(filename_tc, 'w') as file_object:
-        file_object.write("/* %s */\n" % (key_prefix))
+        file_object.write("/* %s */\n" % (sheet_name))
         for index in range(len(keys)):
             key_text = keys[index]
-            value_text = values_tc[index]
+            value_text = values_tc[index].replace("\"", "\\\"").replace("\n", "\\n").replace("\\n\"", "\"")
             # 如果KEY内容为空(""),将被跳过
             if key_text != "":
                 text_line = "\"%s%s\" = \"%s\"" % (key_prefix,key_text, value_text)
                 file_object.write(text_line + ";\n")
-
